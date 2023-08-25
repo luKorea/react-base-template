cc6e7d1c541c858cb83fe85def2606c37ec9f31c
korealu <643949593@qq.com>
2023-08-25 14:52:08 +0800

    feat(korealu): 新增骨架屏组件
    新增骨架屏组件

--- src/views/front-home/index.tsx
+++ src/views/front-home/index.tsx
@@ -1,13 +1,28 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
 import { HomeWrapper } from './styled'
+import Skeleton from '@/base-ui/skeleton'
 
 interface IProps {
   children?: ReactNode
 }
 
 const FrontHomePage: FC<IProps> = () => {
-  return <HomeWrapper>一些组件封装使用</HomeWrapper>
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [loading])
+  return (
+    <>
+      <Skeleton loading={loading} animate={true}></Skeleton>
+      {!loading && <HomeWrapper>FrontHomePage</HomeWrapper>}
+    </>
+  )
 }
 
 export default memo(FrontHomePage)
